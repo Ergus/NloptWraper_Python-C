766a840f262ef3318a5a674deae0e4a3861b0802
Panadestein <rpana92@gmail.com>
2018-06-07 17:20:10 +0200

    Error on objective_func

--- fit_main.py
+++ fit_main.py
@@ -11,14 +11,17 @@ def opt_me(x, grad):
     a, b = x[0], x[1]
     return (a**2 + b - 11)**2 + (a + b**2 - 7)**2
 
+# Set variables 
+
+maxeval = 100
+minrms = 0.01
+tol = 0.0001
+param_values = np.array([0, 0], dtype=np.float64)
+minf = np.array([], dtype=np.float64)
+
 ################################################################
 # NLopt SWIG-python wrapper
 ################################################################
-
-#maxeval = 100
-#minrms = 0.01
-#tol = 0.0001
-param_values = np.array([0, 0], dtype=np.float64)
 
 #opt = nlopt.opt(nlopt.G_MLSL_LDS, 2)
 #opt.set_local_optimizer(nlopt.opt(nlopt.LN_BOBYQA, 2))
@@ -37,15 +40,8 @@ param_values = np.array([0, 0], dtype=np.float64)
 
 # Objective function ported to C 
 
-CMPFUNC = C.CFUNCTYPE(C.c_double, np.ctypeslib.ndpointer(C.c_double, flags="C_CONTIGUOUS"))
+CMPFUNC = C.CFUNCTYPE(C.c_double, np.ctypeslib.ndpointer(C.c_double, flags="C_CONTIGUOUS"), C.c_void_p)
 cmp_opt_me = CMPFUNC(opt_me)
-
-# Parameters ported to C types
-
-maxeval = C.c_int(100)
-minrms = C.c_double(0.01)
-tol = C.c_double(0.0001)
-minf = C.c_double()
 
 # Fucntion prototyping
 
@@ -63,16 +59,14 @@ libfit.nlopt_set_stopval.argtypes = [C.c_void_p, C.c_double]
 libfit.nlopt_set_ftol_abs.argtypes = [C.c_void_p, C.c_double]
 
 libfit.nlopt_optimize.argtypes = [C.c_void_p, np.ctypeslib.ndpointer(C.c_double, flags="C_CONTIGUOUS"),
-                                 C.c_double]
+                                 np.ctypeslib.ndpointer(C.c_double, flags="C_CONTIGUOUS")]
 #libfit.nlopt_optimize.restypes = [np.ctypeslib.ndpointer(C.c_double, flags="C_CONTIGUOUS"), C.c_double]
 
 # Optimization
 
-#opt = libfit.nlopt_create('NLOPT_G_MLSL_LDS', 2)
-#libfit.nlopt_set_local_optimizer(opt, libfit.nlopt_create('NLOPT_LN_BOBYQA', 2))
 opt = libfit.nlopt_create(C.c_int(27), C.c_int(2))
 libfit.nlopt_set_lower_bounds(opt, np.array([-5, -5], dtype=np.float64))
-libfit.nlopt_set_upper_bounds(opt ,np.array([5, 5], dtype=np.float64))
+libfit.nlopt_set_upper_bounds(opt, np.array([5, 5], dtype=np.float64))
 libfit.nlopt_set_min_objective(opt, cmp_opt_me, None)
 libfit.nlopt_set_maxeval(opt, maxeval)
 libfit.nlopt_set_stopval(opt, minrms)
