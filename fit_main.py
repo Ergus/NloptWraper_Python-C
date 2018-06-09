42e4080bebd12b2613c2342d832adabf4de88e91
Jimmy Aguilar Mena <kratsbinovish@gmail.com>
2018-06-09 06:42:48 +0200

    Fixed the script to use the new module.
    The call back function is still needed.

--- fit_main.py
+++ fit_main.py
@@ -1,18 +1,18 @@
-import numpy as np 
-import ctypes as C
-#import nlopt
+#!/usr/bin/env python3
 
-# Load shared library 
-libfit = C.CDLL("./libnlopt.so.0.9.0")
+import numpy as np
+import wnlopt
 
 # We'll optimize Himmelblau's function
-
 def opt_me(x, grad):
     a, b = x[0], x[1]
     return (a**2 + b - 11)**2 + (a + b**2 - 7)**2
 
-# Set variables 
+# Objective function ported to C
+#CMPFUNC = C.CFUNCTYPE(C.c_double, np.ctypeslib.ndpointer(C.c_double, flags="C_CONTIGUOUS"), C.c_void_p)
+#cmp_opt_me = CMPFUNC(opt_me)
 
+# Set variables
 maxeval = 100
 minrms = 0.01
 tol = 0.0001
@@ -38,44 +38,13 @@ minf = np.array([], dtype=np.float64)
 # CTypes wrapper
 ################################################################
 
-# Objective function ported to C 
-
-CMPFUNC = C.CFUNCTYPE(C.c_double, np.ctypeslib.ndpointer(C.c_double, flags="C_CONTIGUOUS"), C.c_void_p)
-cmp_opt_me = CMPFUNC(opt_me)
-
-# Fucntion prototyping
-
-libfit.nlopt_create.argtypes = [C.c_int, C.c_int]
-libfit.nlopt_create.restype = C.c_void_p
-
-libfit.nlopt_set_lower_bounds.argtypes = [C.c_void_p ,
-       np.ctypeslib.ndpointer(C.c_double, flags="C_CONTIGUOUS")]
-libfit.nlopt_set_upper_bounds.argtypes = [C.c_void_p ,
-       np.ctypeslib.ndpointer(C.c_double, flags="C_CONTIGUOUS")]
-
-libfit.nlopt_set_min_objective.argtypes = [C.c_void_p, C.c_void_p, C.c_void_p ]
-libfit.nlopt_set_maxeval.argtypes = [C.c_void_p, C.c_int]
-libfit.nlopt_set_stopval.argtypes = [C.c_void_p, C.c_double]
-libfit.nlopt_set_ftol_abs.argtypes = [C.c_void_p, C.c_double]
-
-libfit.nlopt_optimize.argtypes = [C.c_void_p, np.ctypeslib.ndpointer(C.c_double, flags="C_CONTIGUOUS"),
-                                 np.ctypeslib.ndpointer(C.c_double, flags="C_CONTIGUOUS")]
-#libfit.nlopt_optimize.restypes = [np.ctypeslib.ndpointer(C.c_double, flags="C_CONTIGUOUS"), C.c_double]
-
-# Optimization
-
-opt = libfit.nlopt_create(C.c_int(27), C.c_int(2))
-libfit.nlopt_set_lower_bounds(opt, np.array([-5, -5], dtype=np.float64))
-libfit.nlopt_set_upper_bounds(opt, np.array([5, 5], dtype=np.float64))
-libfit.nlopt_set_min_objective(opt, cmp_opt_me, None)
-libfit.nlopt_set_maxeval(opt, maxeval)
-libfit.nlopt_set_stopval(opt, minrms)
-libfit.nlopt_set_ftol_abs(opt, tol)
-libfit.nlopt_optimize(opt, param_values, minf)
-libfit.nlopt_destroy(opt)
-
-#with open('out', 'w') as outfile:
-#     outfile.write("optimum at " + str(x[0]) + " " + str(x[1]) + "\n")
-#     outfile.write("minimum value = " + str(minf))
-
+# Function prototyping
+opt = wnlopt.PyNlopt(27, 2)
+opt.set_lower_bounds(np.array([-5, -5], dtype=np.float64))
+opt.set_upper_bounds(np.array([5, 5], dtype=np.float64))
+#opt.set_min_objective(cmp_opt_me, None)
+opt.set_maxeval(maxeval)
+opt.set_stopval(minrms)
+opt.set_ftol_abs(tol)
+#opt.optimize(param_values, minf)
 
